c6331e7f8576d33e9c7ac7c547f865edbb0a5db4
Response051 <platoon1277@gmail.com>
2023-09-09 15:11:55 +0100

    ALL Js updates were effected

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("hello world!")
+// Current Day Of the week code.
+const Weekday = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const d = new Date();
+let Day = Weekday[d.getDay()];
+document.getElementById("currentDay").innerHTML = Day;
+
+// Current UTC Time In Milliseconds
+
+let Time = d.getUTCMilliseconds();
+document.getElementById("currentUTC").innerHTML = Time;
