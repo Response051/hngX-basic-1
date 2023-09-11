ed1cd50a1a250cf381541069223c0de38e74164c
Response051 <platoon1277@gmail.com>
2023-09-11 12:09:42 +0100

    the time section was re-updated

--- index.js
+++ index.js
@@ -15,5 +15,10 @@ document.getElementById("currentDay").innerHTML = Day;
 
 // Current UTC Time In Milliseconds
 
-let Time = d.getUTCMilliseconds();
+let time1 = d.getUTCHours();
+let time2 = d.getUTCMinutes();
+let time3 = d.getUTCSeconds();
+let time4 = d.getUTCMilliseconds();
+let Time = `${time1}${time2}${time3}${time4}`;
 document.getElementById("currentUTC").innerHTML = Time;
+console.log(Time);
