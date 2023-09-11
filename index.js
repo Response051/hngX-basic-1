5b08f7b031d5064b4a0a07bd18b69ca8c0789717
Response051 <platoon1277@gmail.com>
2023-09-11 14:35:15 +0100

    the time section was re-updated

--- index.js
+++ index.js
@@ -15,10 +15,26 @@ document.getElementById("currentDay").innerHTML = Day;
 
 // Current UTC Time In Milliseconds
 
-let time1 = d.getUTCHours();
-let time2 = d.getUTCMinutes();
-let time3 = d.getUTCSeconds();
-let time4 = d.getUTCMilliseconds();
-let Time = `${time1}${time2}${time3}${time4}`;
-document.getElementById("currentUTC").innerHTML = Time;
-console.log(Time);
+setInterval(displayMilliseconds, 1000);
+
+function displayMilliseconds(Time) {
+  let time1 = d.getUTCHours();
+  let time2 = d.getUTCMinutes();
+  let time3 = d.getUTCSeconds();
+  let time4 = d.getUTCMilliseconds();
+  Time = `${time1}:${time2}:${time3}:${time4}`;
+  document.getElementById("currentUTC").innerHTML = Time;
+}
+
+// function getCurrentUTCTimeInMilliseconds() {
+//   const now = new Date();
+//   return now.getUTCMilliseconds();
+// }
+
+// function displayCurrentUTCTime() {
+//   const utcTimeInMilliseconds = getCurrentUTCTimeInMilliseconds();
+//   document.getElementById("currentUTC").innerHTML = utcTimeInMilliseconds;
+// }
+
+// // Display the current UTC time and update it every second (1000 milliseconds)
+// setInterval(displayCurrentUTCTime, 1000);
